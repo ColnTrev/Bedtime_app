c549c54edbbedba2c0ebf6238f159606b353e0b0
Collin Gordon <colntrev@gmail.com>
2017-01-29 10:52:53 -0800

    Added in some dummy text and wired more buttons

--- app/src/main/java/colntrev/test/ResultActivity.java
+++ app/src/main/java/colntrev/test/ResultActivity.java
@@ -1,7 +1,10 @@
 package colntrev.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+    }
+
+    public void goTrend(View view) {
+        Intent trend = new Intent(this, TrendActivity.class);
+        startActivity(trend);
+    }
+
+    public void goUpdate(View view) {
+        Intent update = new Intent(this, UpdateActivity.class);
+        startActivity(update);
     }
 }
