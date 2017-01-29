33d21855d0ffbdc3a652c0061acc08bcf47169c7
Collin Gordon <colntrev@gmail.com>
2017-01-28 17:11:44 -0800

    Wired button to Analyze Activity

--- app/src/main/java/colntrev/test/AnalyzeActivity.java
+++ app/src/main/java/colntrev/test/AnalyzeActivity.java
@@ -1,7 +1,9 @@
 package colntrev.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AnalyzeActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AnalyzeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyze);
+    }
+
+    public void backToMain(View view) {
+        Intent homeScreen = new Intent(this, MainActivity.class);
+        final int result = 1;
+        startActivity(homeScreen);
     }
 }
--- app/src/main/java/colntrev/test/MainActivity.java
+++ app/src/main/java/colntrev/test/MainActivity.java
@@ -1,7 +1,9 @@
 package colntrev.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goAnalyze(View view) {
+        Intent analyze = new Intent(this,AnalyzeActivity.class);
+        final int result = 1;
+        startActivity(analyze);
     }
 }
